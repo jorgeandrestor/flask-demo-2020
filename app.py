c7f9e51dcd3e4f2cee799907ba66f5c5b644c437
Jorge <jorgeandrestor@gmail.com>
2020-06-10 15:48:43 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template #allows us to send over an html document as one concrete unit 
+from flask import request, redirect
+from model import get_breakfast_rating
+from datetime import datetime
 
 # -- Initialization section --
 app = Flask(__name__)
 
 
-# -- Routes section --
+# -- Routes section --  if user requests / or /index, it will take them to the hello world site
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html", time=datetime.now())
+
+@app.route('/results', methods=['GET','POST'])
+def results():
+    if request.method == 'POST':
+        print(request.form)
+        user_breakfast=request.form["breakfast"]
+        user_name = request.form['nickname']
+        breakfast_rating = get_breakfast_rating(user_breakfast)
+        
+        return render_template("breakfast.html",user_breakfast=user_breakfast, user_name=user_name, breakfast_rating=breakfast_rating)
+    else:
+        return redirect('/') #redirects are a way to handle user error by sending them to a specific page
+
+@app.route('/secret')
+def secret():
+    return "You found the secret page."
+
+
+#export FLASK_DEBUG=1 means turn debug mode on
